eeebb2def7bc56b8651efdb9fec672255c884907
Omohemma <emmaomoh@gmail.com>
2021-11-11 10:44:58 +0100

    Write Asynchronous Function Tests

--- script2.test.js
+++ script2.test.js
@@ -1,18 +1,18 @@
 const fetch = require('node-fetch')
 const swapi = require('./script2')
 
-it('calls swapi', function () {
+it('calls swapi',  () => {
   expect.assertions(1)
   return swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87)
   })
-});
+})
 
 
-it('calls swapi promise', function () {
+it('calls swapi promise',() => {
   expect.assertions(2)
   return swapi.getPeoplePromise(fetch).then(data => {
     expect(data.count).toEqual(87)
     expect(data.results.length).toBeGreaterThan(5)
   })
-});
+})
